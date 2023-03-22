e232935dce801e8e387c75cef79fda6f6bbd5756
kamazik2305 <viktorpohodko96@gmail.com>
2023-03-22 17:08:50 +0300

    add matrix again

---
+++ src/Components/MatrixInput.js
@@ -0,0 +1,66 @@
+import React, { useState } from 'react'
+
+function MatrixInput() {
+    const [size, setSize] = useState(2) // начальный размер матрицы 
+    const [matrix, setMatrix] = useState(
+        Array.from({ length: size }, () => Array.from({ length: size }, () => ''))
+    )
+
+    const handleMatrixInputChange = (event, row, col) => {
+        const value = event.target.value
+        const newMatrix = [...matrix]
+        if (row == col) { matrix[row][col] = 1 }
+        else {
+            matrix[row][col] = Number(value)
+            matrix[col][row] = 1/value
+        }
+        setMatrix(newMatrix)
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        console.log(matrix.flat()) // выводит в консоль значения всех инпутов в одном массиве } 
+    }
+
+    const renderMatrixInputs = () => {
+        const matrixInputs = []
+        for (let i = 0; i < size; i++) {
+            const rowInputs = []
+            for (let j = 0; j < size; j++) {
+                rowInputs.push(
+                    <input
+                        key={`${i}-${j}`}
+                        type="number"
+                        value={matrix[i][j]}
+                        onChange={(event) => handleMatrixInputChange(event, i, j)}
+                    />
+                )
+            }
+            matrixInputs.push(<div key={i}>{rowInputs}</div>)
+        }
+        return matrixInputs
+    }
+
+    const handleSizeChange = (event) => {
+        const value = event.target.value
+        setSize(value)
+        setMatrix(
+            Array.from({ length: value }, () =>
+                Array.from({ length: size }, () => '')
+            )
+        )
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <label>
+                Rows:
+                <input type="number" value={size} onChange={handleSizeChange} />
+            </label>
+            {renderMatrixInputs()}
+            <button type="submit">Submit</button>
+        </form>
+    )
+}
+
+export default MatrixInput
